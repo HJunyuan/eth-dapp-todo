9141c707bd11f62f7076538b796c1391f19614e5
Kyle Huang Junyuan <kyle@hjunyuan.com>
2020-03-09 00:16:46 +0800

    Added mapping and createTask()

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -1,6 +1,24 @@
-pragma solidity >=0.5.0 <0.7.0;
+pragma solidity ^0.6.0;
 
 
 contract TodoList {
 	uint256 public taskCount = 0;
+
+	struct Task {
+		uint256 id;
+		string content;
+		bool completed;
+	}
+
+	// Key-value pair (Dictionary)
+	mapping(uint256 => Task) public tasks;
+
+	constructor() public {
+		createTask("Task 1");
+	}
+
+	function createTask(string memory _content) public {
+		taskCount++;
+		tasks[taskCount] = Task(taskCount, _content, false);
+	}
 }
